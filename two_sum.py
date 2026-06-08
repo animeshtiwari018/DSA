ba9d5ef41367b1f2eea81633cae5fdec62481516
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-06-08 14:41:31 +0530

    Add the question

---
+++ two_sum.py
@@ -0,0 +1,16 @@
+""" Two Sum II - Input Array Is Sorted
+
+Given a 1-indexed array of integers numbers that is sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+
+Return the indices of the two numbers (1-indexed) as an integer array [index1, index2] where:
+
+             1 <= index1 < index2 <= numbers.length 
+             numbers[index1 - 1] + numbers[index2 - 1] == target
+
+You may assume that each input has exactly one solution, and you may not use the same element twice. """
+
+
+
+''' Brute Force '''
+
+
