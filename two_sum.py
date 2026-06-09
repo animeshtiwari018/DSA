8656047337a9f4e449894be403b5011d064ab5ab
Animesh Tiwari <animeshtiwari178@gmail.com>
2026-06-09 14:52:09 +0530

    add optimal solution of Two Sum

--- two_sum.py
+++ two_sum.py
@@ -25,3 +25,30 @@ target = 8
 result = two_sum()
 print(result)"""
 #---------------------------------------------------#
+
+
+''' Optimize solution '''
+
+def two_sum(numbers, target):
+    left = 0
+    right = len(numbers) - 1
+    
+    while left < right:
+        current_sum = numbers[left] + numbers[right]
+        
+        if current_sum == target:
+            return [left + 1, right + 1]
+        
+        elif current_sum < target:
+            left += 1
+            
+        else: 
+            right -= 1
+
+
+numbers = [1, 4, 7, 9, 11]
+target = 8
+result = two_sum(numbers, target)
+
+print(result)
+ 
